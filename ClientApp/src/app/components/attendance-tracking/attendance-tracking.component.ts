56ac5a92fbf9bd754e9c517ab78bdab03cd25866
Ihor <sskpr.igor@gmail.com>
2023-10-11 22:58:31 +0300

    added angular model

--- ClientApp/src/app/components/attendance-tracking/attendance-tracking.component.ts
+++ ClientApp/src/app/components/attendance-tracking/attendance-tracking.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { Schedule } from 'src/app/models/schedule.model';
 
 @Component({
   selector: 'app-attendance-tracking',
   templateUrl: './attendance-tracking.component.html',
   styleUrls: ['./attendance-tracking.component.css']
 })
-export class AttendanceTrackingComponent {
+export class AttendanceTrackingComponent implements OnInit {
+  schedule: Schedule = {
+    course: 1,
+    fullName: '',
+    group: '',
+    date: new Date()
+  }
+
+  ngOnInit(): void { }
+  
+  constructor() {}
+
+  getSchedule(): void {
+    console.log(this.schedule);
+  }
 }
---
+++ ClientApp/src/app/models/schedule.model.ts
@@ -0,0 +1,6 @@
+export interface Schedule {
+    course: number,
+    fullName: string,
+    group: string
+    date: Date
+}
